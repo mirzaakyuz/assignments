a00531c69d3d5762aa7b11cfb37f83912f8d8c94
mirza <mrizaakyuz@gmail.com>
2020-07-19 12:27:57 +0200

    fizzbuzz solved

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+
+a = list(range(101))
+
+for i in a:
+    if i % 15 == 0:
+        print('FizzBuzz')
+    elif i % 5 == 0:
+        print('Buzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    else:
+        print(i)
+
